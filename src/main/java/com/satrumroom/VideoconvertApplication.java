9091ee98b28a33422e431ce03ee1c5e1f1e4769f
templar315 <AndrewPogorelov@gmail.com>
2018-04-04 17:16:49 +0300

    Initial commit

---
+++ src/main/java/com/satrumroom/VideoconvertApplication.java
@@ -0,0 +1,12 @@
+package com.satrumroom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoconvertApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VideoconvertApplication.class, args);
+	}
+}
---
+++ src/test/java/com/satrumroom/VideoconvertApplicationTests.java
@@ -0,0 +1,16 @@
+package com.satrumroom;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class VideoconvertApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
